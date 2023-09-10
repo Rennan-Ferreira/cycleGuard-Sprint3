044833c3708b300ad535283ea733b209498ec1f0
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-09-10 20:01:18 -0300

    Arrumando sobreposição de css em outra página, alterando classes para evitar o mesmo

--- cycle-guard-sprt3/src/routes/DadosBicicleta/DadosBicicleta.jsx
+++ cycle-guard-sprt3/src/routes/DadosBicicleta/DadosBicicleta.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './DadosBicicleta.css'
+
 export default function DadosBicicleta() {
     return (
         <div className="bike-data-form">
--- cycle-guard-sprt3/src/routes/DadosCliente/ClienteDados.jsx
+++ cycle-guard-sprt3/src/routes/DadosCliente/ClienteDados.jsx
@@ -1,41 +1,41 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import React from 'react';
+import './ClienteDados.css'
 
 export default function DadosCliente() {
     return (
-        <div className="cliente-form">
-            <div className="header">
-                <button className="button-black">&#8592; Voltar</button>
+        <div className="cliente-data-form">
+            <div className="cliente-data-header">
+                <button className="cliente-back-button">&#8592; Voltar</button>
             </div>
             <form>
-                <div className="section">
-                    <div className='title'>
+                <div className="cliente-form-section">
+                    <div className='cliente-title'>
                         <h2>PARA CONTINUAR PRECISAMOS CONFIRMAR ALGUNS DADOS, OK?</h2>
                     </div>
-                    <div className="form-group">
+                    <div className="cliente-form-group">
                         <label htmlFor="nome">Nome:</label>
                         <input type="text" id="nome" name="nome" required />
                     </div>
-                    <div className="form-group">
+                    <div className="cliente-form-group">
                         <label htmlFor="cpf">CPF:</label>
                         <input type="text" id="cpf" name="cpf" required />
                     </div>
-                    <div className="form-group">
+                    <div className="cliente-form-group">
                         <label htmlFor="idade">Idade:</label>
                         <input type="text" id="idade" name="idade" required />
                     </div>
-                    <div className="form-group">
+                    <div className="cliente-form-group">
                         <label htmlFor="telefone">Telefone:</label>
                         <input type="text" id="telefone" name="telefone" required />
                     </div>
-                    <div className="form-group">
+                    <div className="cliente-form-group">
                         <label htmlFor="email">Email:</label>
                         <input type="text" id="email" name="email" required />
                     </div>
                 </div>
-                <button type="submit">Continuar</button>
+                <button type="submit" className="cliente-continue-button">Continuar</button>
             </form>
         </div>
     );
-};
+}
