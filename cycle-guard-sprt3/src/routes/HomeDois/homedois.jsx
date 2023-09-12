4deb2539506de457bcc717ff5e1ee440a3d4b85f
Harthley01 <rm551359@fiap.com.br>
2023-09-12 15:19:39 -0300

    finalizando footer home 2

--- cycle-guard-sprt3/src/routes/HomeDois/homedois.jsx
+++ cycle-guard-sprt3/src/routes/HomeDois/homedois.jsx
@@ -53,10 +53,14 @@ export default function Home(){
             <footer>
                 <div className="footer-container">
                     <div className="cycle-footer">
-                        
+                        <h4>Cycle Guard</h4>
+                        <p>Criada em 2023</p>
+                        <p>São Paulo- Brasil</p>
                     </div>
                     <div className="menu-footer">
-                        
+                        <h4>Menu</h4>
+                        <p>Seguros Disponíveis</p>
+                        <p>Sobre nós</p>
                     </div>
                     <div className="atendinmento-footer">
                         <h5>Atendimento 100% online</h5>
