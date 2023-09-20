96c87e4691bb097bd537b2dd812d0120e81ee75d
jaisycibele <jaisycibele@gmail.com>
2023-09-20 00:03:04 -0300

    Fazendo integração do python no react

--- src/routes/CameraPage/CameraPage.jsx
+++ src/routes/CameraPage/CameraPage.jsx
@@ -1,39 +1,84 @@
-import React, { useState } from 'react';
-import Webcam from 'react-webcam';
+import React, { useState, useRef, useEffect } from 'react';
 import './CameraPage.css';
 
-function CameraPage() {
-  const [isCameraOpen, setIsCameraOpen] = useState(false);
+function ObjectDetectionPage() {
+  const [isDetectionStarted, setIsDetectionStarted] = useState(false);
+  const videoRef = useRef(null);
+  const detectionsCanvasRef = useRef(null);
+  const ctxRef = useRef(null);
 
-  const openCamera = () => {
-    setIsCameraOpen(true);
+  useEffect(() => {
+    const video = videoRef.current;
+    const detectionsCanvas = detectionsCanvasRef.current;
+    const ctx = detectionsCanvas.getContext('2d');
+
+    video.addEventListener('play', () => {
+      const drawDetections = () => {
+        if (!video.paused && !video.ended) {
+          ctx.clearRect(0, 0, detectionsCanvas.width, detectionsCanvas.height);
+          ctx.drawImage(video, 0, 0, detectionsCanvas.width, detectionsCanvas.height);
+          requestAnimationFrame(drawDetections);
+        }
+      };
+
+      drawDetections();
+    });
+  }, []);
+
+  const handleStartDetection = () => {
+    // Fazer uma solicitação para iniciar a detecção no servidor Python
+    fetch('http://localhost:8080/start_detection')
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 'Detection started') {
+          setIsDetectionStarted(true);
+        }
+      })
+      .catch((error) => {
+        console.error('Erro ao iniciar a detecção:', error);
+      });
   };
 
-  const closeCamera = () => {
-    setIsCameraOpen(false);
+  const handleStopDetection = () => {
+    // Fazer uma solicitação para parar a detecção no servidor Python
+    fetch('http://localhost:8080/stop_detection')
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 'Detection stopped') {
+          setIsDetectionStarted(false);
+        }
+      })
+      .catch((error) => {
+        console.error('Erro ao parar a detecção:', error);
+      });
   };
 
   return (
-    <div className="camera-page">
-      <h2>Iniciando Vistoria </h2>
-      <div className={`camera-container ${isCameraOpen ? 'camera-open' : ''}`}>
-        {isCameraOpen ? (
-          <>
-            <Webcam
-              className="webcam"
-              videoConstraints={{
-                width: 1280,
-                height: 720,
-              }}
-            />
-            <button onClick={closeCamera}>Fechar Câmera</button>
-          </>
-        ) : (
-          <button onClick={openCamera}>Abrir Câmera</button>
-        )}
+    <div className='camera-page'>
+      <h1 className='titulo-camera'>Detecção de Objetos</h1>
+      <button
+        id="startButton"
+        onClick={handleStartDetection}
+        disabled={isDetectionStarted}
+        style={{ display: isDetectionStarted ? 'none' : 'block' }}
+      >
+        Iniciar Detecção
+      </button>
+      <button
+        id="stopButton"
+        onClick={handleStopDetection}
+        disabled={!isDetectionStarted}
+        style={{ display: isDetectionStarted ? 'block' : 'none' }}
+      >
+        Parar Detecção
+      </button>
+      <div className="webcam">
+        <img ref={videoRef} src="http://localhost:8080/video_feed" alt="Detecção de objetos" className='deteccao'/>
+        <canvas ref={detectionsCanvasRef}></canvas>
       </div>
+      
     </div>
   );
 }
 
-export default CameraPage;
+export default ObjectDetectionPage;
